c1148cb4fc34e5b72097b6d5738777431b969549
FATMA YAŞAR <232325012@ogrenci.harran.edu.tr>
2025-02-05 17:02:36 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; } 
+        public string CategoryNmae { get; set; }
+
+        public bool CategoryStatus { get; set; }
+    }
+}
+
+// İlk olarak sınıf erişimini public olarak değiştirdik çünkü bu sınıfın diğer sınıflardan veya katmanlardan erişilmesi gerekecek.
+// public int MyProperty { get; set; } --> prop + tab ile direk oluşturabiliriz.
+// get: veriyi almak
+// set: veriyi ayarlamak
+
+// *** Code First yaklaşımı içeisinde Id sütununun birincil anahtar ve otomatik artan olduğunu bildirmek adına sınıfın ismi birebir aynı yazılmalı ve sonuna Id getirilmelidir.
