54fbe844da70c536f77ddc55ea3f0967d1109ed8
FATMA YAŞAR <232325012@ogrenci.harran.edu.tr>
2025-02-06 22:07:54 +0300

    Form İşlemleri ve Entity Framework Metotları

--- CSharpEgitimKampi301.EFProject/Form1.Designer.cs
+++ CSharpEgitimKampi301.EFProject/Form1.Designer.cs
@@ -28,13 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnGetByID = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rehber Id: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(122, 28);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(218, 22);
+            this.txtId.TabIndex = 1;
+            // 
+            // btnList
+            // 
+            this.btnList.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnList.Location = new System.Drawing.Point(122, 143);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(218, 35);
+            this.btnList.TabIndex = 2;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = false;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(416, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(846, 360);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(122, 64);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(218, 22);
+            this.txtName.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Rehber Adı: ";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(122, 101);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(218, 22);
+            this.txtSurname.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Rehber Soyadı: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnAdd.Location = new System.Drawing.Point(122, 197);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(218, 35);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnDelete.Location = new System.Drawing.Point(122, 248);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(218, 35);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnUpdate.Location = new System.Drawing.Point(122, 301);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(218, 35);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnGetByID
+            // 
+            this.btnGetByID.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnGetByID.Location = new System.Drawing.Point(122, 356);
+            this.btnGetByID.Name = "btnGetByID";
+            this.btnGetByID.Size = new System.Drawing.Size(218, 35);
+            this.btnGetByID.TabIndex = 13;
+            this.btnGetByID.Text = "Id\'ye Göre Getir";
+            this.btnGetByID.UseVisualStyleBackColor = false;
+            this.btnGetByID.Click += new System.EventHandler(this.btnGetByID_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1309, 579);
+            this.Controls.Add(this.btnGetByID);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Rehber İşlemleri";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnGetByID;
     }
 }
 
--- CSharpEgitimKampi301.EFProject/Form1.cs
+++ CSharpEgitimKampi301.EFProject/Form1.cs
@@ -16,5 +16,66 @@ namespace CSharpEgitimKampi301.EFProject
         {
             InitializeComponent();
         }
+
+
+        EgitimKampiEFTravelDbEntities db = new EgitimKampiEFTravelDbEntities(); // Tablo nesnesini tek tek yazmak yerine süslü parantezlerin dışına ekleyerek bütün süslü parantezler blogları içinden erişim sağlayabiliriz.
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+
+            var values = db.Guide.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Guide guide =new Guide();
+            guide.GuideName =txtName.Text;
+            guide.GuideSurname =txtSurname.Text;
+            db.Guide.Add(guide);
+            db.SaveChanges();
+            MessageBox.Show("Rehber Bilgisi Başarıyla Eklendi");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Id değeri benzersiz olduğu için silme işlemlerini Id üzerinden gerçekleştireceğiz.
+
+            int id = int.Parse(txtId.Text);
+            var removeValue = db.Guide.Find(id);
+            db.Guide.Remove(removeValue);
+            db.SaveChanges();
+            MessageBox.Show("Rehber Bilgisi Başarıyla Silindi");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // Güncelleme işlemi iki adımdan oluşur.
+            // 1. adım : Güncellenecek olan Id bulunur.
+            // 2. adım : verilen şarta göre güncellemeyi gerçekleştirir.
+
+            int id= int.Parse(txtId.Text);
+            var updateValue = db.Guide.Find(id);
+            updateValue.GuideName =txtName.Text;
+            updateValue.GuideSurname=txtSurname.Text;
+            db.SaveChanges();
+            MessageBox.Show("Rehber Bilgisi Başarıyla Güncellendi", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btnGetByID_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var values = db.Guide.Where(x =>x.GuideId == id).ToList(); // Değerin getirilme şartı; girilen ıd ile eşleşen ıd yi getirir.
+            dataGridView1.DataSource = values; // değeri getirir.
+        }
     }
 }
